f68ed5377fa9fd9f1403b27365213155fa23279e
devkhk-air <kimkh1517@gmail.com>
2022-04-14 01:30:45 +0900

    spark structured streaming study review

---
+++ review/streaming-review.py
@@ -0,0 +1,21 @@
+from socket import socket
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("stream-words-count").getOrCreate()
+
+lines_df = spark.readStream.format('socket')\
+                            .option('host', 'localhost')\
+                            .option("port", "9999").load()
+
+words_df = lines_df.select(expr("explode(split(value, ' ')) as word"))
+
+counts_df = words_df.groupBy('word').count()
+
+word_count_query = counts_df.writeStream.format('console')\
+                                        .outputMode('complete')\
+                                        .option('checkpointLocation', ".checkpoint")\
+                                        .start()
+                                
+word_count_query.awaitTermination()
