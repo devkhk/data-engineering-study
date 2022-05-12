54d42e3ea13550e104c5cd2428c9cd80ca9c8f2a
devkhk-air <kimkh1517@gmail.com>
2022-05-12 21:30:38 +0900

    json형식의 데이터 카프카로 전달

---
+++ flink/09_kafka_sink/json_producer.py
@@ -0,0 +1,25 @@
+#
+# 플링크에 데이터를 전달하기 위한 kafka producer - json
+#
+
+from kafka import KafkaProducer
+import json
+
+# Spark,1
+# Airflow,2
+# Kafka,3
+# Flink,4
+
+data = [
+    {"framework":"Spark", "chapter":"1"},
+    {"framework":"Airflow", "chapter":"2"},
+    {"framework":"Kafka", "chapter":"3"},
+    {"framework":"Flink", "chapter":"4"},
+]
+
+brokers = ["localhost:9092"]
+producer = KafkaProducer(bootstrap_servers=brokers)
+
+for d in data:
+    producer.send("example-source", json.dumps(d).encode("utf-8"))
+producer.flush()
