a67cb3cd2e2b4e522e1f0b7d12af250f8d33deb9
devkhk-air <kimkh1517@gmail.com>
2022-05-12 21:23:44 +0900

    플링크 버전에 따른 execute명령어 삭제 이유

--- flink/07_kafka_source/flink_kafka_consumer.py
+++ flink/07_kafka_source/flink_kafka_consumer.py
@@ -32,10 +32,10 @@ t_env.get_config().get_configuration().set_string(
 
 schema = SimpleStringSchema()
 kafka_consumer = FlinkKafkaConsumer(
-    topics="flink-test2",
+    topics="flink-test",
     deserialization_schema=schema,
     properties={
-        "bootstrap-server" : "localhost:9092",
+        "bootstrap.servers" : "localhost:9092",
         "group.id": "test_group"
     }
 )
@@ -57,21 +57,24 @@ t_env.execute_sql(
 
 # 데이터 스트림이 올때 테이블을 만들고 insert 해준다.
 table = t_env.from_data_stream(ds) 
-# table.execute_insert("blackhole")
+table.execute_insert("blackhole").wait()
 
-
-## flink 1.15v 부터 사용되지 않는 메서드가 많이 생김. 아래 코드는 확실치 않고 후에 확인 해 봐야함.
+# v1.15 릴리즈에서 테이블 환경에서 execute , insertInto 메서드를 삭제했다.
+# https://nightlies.apache.org/flink/flink-docs-release-1.15/release-notes/flink-1.15/#remove-pre-flip-84-methods
+# 이유는 테이블 환경과 다른 설정된 환경이 같이 구축되어 있을 때 execute 메서드를 실행했을 경우,
+# table환경과 다른 환경중 어떤 환경에서 execute가 되는지 불분명해지는 문제가 있다.
+# 
 # https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=134745878
-
-
-t_stmt_set = t_env.create_statement_set()
-
-sink_descriptor = TableDescriptor.for_connector("blackhole")\
-                                 .schema(Schema.new_builder()\
-                                                .column("data", DataTypes.STRING())\
-                                                .build())\
-                                 .build()
-t_stmt_set.add_insert(sink_descriptor, table)
-
-result = t_stmt_set.execute()
-print(result.print())
+# [번역]
+# 불분명한 플링크 테이블 프로그램 트리거 포인트. 
+# 'TableEnvironment.execute()`와 'StreamExecutionEnvironment.execute()` 
+# 모두 Flink 테이블 프로그램 실행을 트리거할 수 있습니다. 
+# 그러나 TableEnvironment를 사용하여 Flink 테이블 프로그램을 구축하는 경우, 
+# StreamExecutionEnvironment 인스턴스를 가져올 수 없기 때문에 
+# 'TableEnvironment.execute()`를 사용하여 실행을 트리거해야 합니다. 
+# StreamTableEnvironment를 사용하여 Flink 테이블 프로그램을 구축하는 경우,
+#  둘 다 실행을 트리거할 수 있습니다. 
+# 테이블 프로그램을 DataStream 프로그램으로 변환하는 경우(StreamExecutionEnvironment.toAppendStream/toRetractStream 사용),
+#  둘 다 사용하여 실행을 트리거할 수도 있습니다. 
+# 그래서 어떤 '실행' 방법을 사용해야 하는지 설명하기 어렵다. 
+# StreamTableEnvironment와 마찬가지로, BatchTableEnvironment도 같은 문제가 있다.
