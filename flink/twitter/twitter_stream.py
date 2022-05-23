ca4399a4f19919d2632a7dc5a06ee7f50a410c78
devkhk-air <kimkh1517@gmail.com>
2022-05-24 00:00:43 +0900

    tweepy로 스트림 규칙 만들기

--- flink/twitter/twitter_stream.py
+++ flink/twitter/twitter_stream.py
@@ -13,9 +13,22 @@ class ProcessStream(tweepy.StreamingClient):
     def on_data(self, raw_data):
         print(raw_data)
 
+def delete_all_rules(rules):
+    # rules[0] # StreamRules Data
+    # Stream Data : value, tag, id 순
+    ids = list(map(lambda rule: rule[2], rules[0]))
+    client.delete_rules(ids=ids)
+    # print("clear")
 
-twitter_stream = ProcessStream(bearer_token=BEARER_TOKEN)
 
-# filter() 스트림을 연다는 뜻의 함수 
-twitter_stream.filter()
+client = ProcessStream(BEARER_TOKEN)
+rules = client.get_rules()
+delete_all_rules(rules)
+
+
+# {"value": "BTS has:images -is:retweet", "tag": "BTS"},
+
+# stream.add_rules(tweepy.StreamRule("Twitter"))
+# stream.add_rules(tweepy.StreamRule(value="BTS has:images -is:retweet", tag="BTS"))
+# stream.filter()
 
