e9fbd8a0550426f27fefc1a55c19cd00f5d2a03a
devkhk-air <kimkh1517@gmail.com>
2022-05-24 22:25:04 +0900

    kafka - flink | stream data window processing

---
+++ flink/twitter/twitter_word_count.py
@@ -0,0 +1,91 @@
+#
+# 트위터 스트림을 통해 카프카로 전송한 데이터의 글자수를 파악하는 모듈
+#
+import os
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import StreamTableEnvironment, EnvironmentSettings, DataTypes
+from pyflink.table.udf import udf
+
+env = StreamExecutionEnvironment.get_execution_environment()
+env.set_parallelism(1)
+setttings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+t_env = StreamTableEnvironment.create(env, environment_settings=setttings)
+
+## 카프카 connector jar 경로 설정
+kafka_jar_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "../",
+    "flink-sql-connector-kafka-1.15.0.jar"
+)
+t_env.get_config().get_configuration().set_string(
+    "pipeline.jars", f"file://{kafka_jar_path}"
+)
+
+# CONVERT_TZ(created_at, 'UTC', 'Asia/Seoul')
+# WATERMARK FOR ts AS ts - INTERVAL '5' SECOND
+
+source_query = """
+    CREATE TABLE tweets (
+        text STRING,
+        created_at TIMESTAMP(3),
+        WATERMARK FOR created_at AS created_at - INTERVAL '5' SECOND
+    ) WITH (
+        'connector' = 'kafka',
+        'topic' = 'korean-tweets',
+        'properties.bootstrap.servers' = 'localhost:9092',
+        'properties.group.id' = 'tweet-group',
+        'format' = 'json',
+        'json.timestamp-format.standard' = 'ISO-8601',
+        'scan.startup.mode' = 'latest-offset'
+    )
+"""
+
+sink_query = """
+    CREATE TABLE sink (
+        word_count STRING,
+        w_start TIMESTAMP(3),
+        w_end TIMESTAMP(3)
+    ) WITH (
+        'connector' = 'print'
+    )
+"""
+
+# sink_query = """
+#     CREATE TABLE sink (
+#         word_count STRING,
+#         created_at TIMESTAMP(3)
+#     ) WITH (
+#         'connector' = 'print'
+#     )
+# """
+
+# 소스, 싱크 테이블 생성
+t_env.execute_sql(source_query)
+t_env.execute_sql(sink_query)
+
+# t_env.from_path("tweets").execute_insert("sink").wait()
+
+windowed = t_env.sql_query("""
+    SELECT
+        text,
+        HOP_START(created_at, INTERVAL '2' SECONDS, INTERVAL '10' SECONDS) AS w_start,
+        HOP_END(created_at, INTERVAL '2' SECONDS, INTERVAL '10' SECONDS) AS w_end
+    FROM tweets
+    GROUP BY
+        HOP(created_at, INTERVAL '2' SECONDS, INTERVAL '10' SECONDS),
+        text
+""")
+
+@udf(result_type=DataTypes.STRING())
+def word_count(data):
+    # data : text
+    word_list = data.split()
+    counter = {}
+    for word in word_list:
+        if word not in counter:
+            counter[word] = 1
+        else:
+            counter[word] += 1
+    return str(counter)
+
+res = windowed.select(word_count(windowed.text).alias('word_count'), windowed.w_start, windowed.w_end)
+res.execute_insert("sink").wait()
